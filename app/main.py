5ac8f600b042b3059fec85d9a463b456a8632852
Andrés Ugarte <oscaran2109@gmail.com>
2024-12-01 09:31:44 -0600

    adding files

--- app/main.py
+++ app/main.py
@@ -1,28 +1,16 @@
 import utils#Este es el modulo que nosotros creamos
+import readCSV
+import charts
 
-data=[
-    {
-        'Country':'Colombia',
-        'Population':300
-    },
-    {
-        'Country':'Mexico',
-        'Population':200
-    },
-]
 
 def run():
-
-    keys,values = utils.getPopulation()
-    print(keys,values)
-
-
-
-
-    country = input('Type the country =>')
-
+    data = readCSV.readCSV('./app/data.csv') #Usando nuestro modulo readCSV para que nos de la data que tiene
+    country = input('Type Country => ')
     result = utils.populationByCountry(data,country)
-    print(result)
+    if len(result)>0:
+        country = result[0]
+        labels,values = utils.getPopulation(country)
+        charts.generateBarChart(labels,values)
 
 #Este if nos ayuda a ejecutar el archivo directamente desde la terminal
 if __name__ == '__main__':
--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,24 @@
 #Creando nuestro propio modulo
 
-def getPopulation():
-    keys= ['Col','Bol']
-    values= [300,400]
+def getPopulation(country_dict):
+    population_dict= {
+        '2022':int(country_dict['2022 Population']),
+        '2020':int(country_dict['2020 Population']),
+        '2015':int(country_dict['2015 Population']),
+        '2010':int(country_dict['2010 Population']),
+        '2000':int(country_dict['2000 Population']),
+        '1990':int(country_dict['1990 Population']),
+        '1980':int(country_dict['1980 Population']),
+        '1970':int(country_dict['1970 Population']),
+    }
+    labels =population_dict.keys()
+    values = population_dict.values()
 
-    return keys,values
+    return labels,values
 
 
 #Esta funcion recibe una lista que contiene diccionarios "data"
 
 def populationByCountry(data,country):
-    result = list(filter(lambda item : item['Country']== country,data))
+    result = list(filter(lambda item : item['Country/Territory']== country,data))
     return result
